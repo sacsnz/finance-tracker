3fa8f27ee36c9baaa135e940b2d43301d9427368
sacsnz <EMAIL_ADDRESS>
2019-10-10 10:08:39 +0000

    repaired, push to heroku

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,7 @@
 class StocksController < ApplicationController
    
    def search
-      Stock.new_from_lookup(params[:stock])
+      @stock = Stock.new_from_lookup(params[:stock])
       render json: @stock
    end
     
