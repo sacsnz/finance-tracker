95d1e2de2a70ada349fba63b8caaf2024cf81882
sacsnz <EMAIL_ADDRESS>
2019-10-17 09:14:15 +0000

    Added stock views and actions to my portfolio

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,10 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
+  
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
  
  
   def self.find_by_ticker(ticker_symbol)
