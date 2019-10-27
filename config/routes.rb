e67b2951703a90b858d1bb6c7ed6cdb183a9ff45
sacsnz <EMAIL_ADDRESS>
2019-10-27 06:31:00 +0000

    Update user views and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => 'user/registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root 'users#my_portfolio'
+  root 'welcome#index'
   
   get 'my_portfolio', to: 'users#my_portfolio'
   
   get 'search_stocks', to: 'stocks#search'
   
   resources :user_stocks, only: [:create, :destroy]
+  
   
 end
